11afa92fdd92231d0980d7689ad472a60a869202
KaviKurtosys <kaveshan.kistan@kurtosys.com>
2023-08-27 14:57:54 +0200

    Clients accessible

--- src/components/Clients/Clients.jsx
+++ src/components/Clients/Clients.jsx
@@ -6,16 +6,19 @@ import Header from "../Header/Header";
 
 const Clients = ({ className }) => {
   return (
-    <div className={className}>
+    <div className={className} role="complementary">
       <Header
         title={"Behold a Few Among the Many Companies Embracing Our Alumni."}
         subtitle={"Our Alumni Thrive in Top Global Tech Enterprises"}
       />
-      <Row className="logos-row" justify="center">
+      <Row className="logos-row" justify="center" role="list">
         {Object.values(logos).map((logo, index) => (
-          <Col xs={6} sm={3} md={3} lg={3} key={index}>
+          <Col xs={6} sm={3} md={3} lg={3} key={index} role="listitem">
             <div className="logo-container">
-              <img src={logo} alt={`Client logo ${index}`} />
+              <img 
+                src={logo} 
+                alt={`Logo of client number ${index + 1}`}
+              />
             </div>
           </Col>
         ))}
