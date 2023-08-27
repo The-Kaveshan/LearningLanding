079174ede6bdfc9057cb926a8dd224cd4048b016
KaviKurtosys <kaveshan.kistan@kurtosys.com>
2023-08-27 18:04:50 +0200

    Video Accessibility

--- src/components/VideoBanner/VideoBanner.jsx
+++ src/components/VideoBanner/VideoBanner.jsx
@@ -1,6 +1,7 @@
 import React, { useRef, useState } from "react";
 import "./VideoBanner.scss";
 import Video from "../../assets/videos/hyperion.mp4";
+
 function VideoBanner() {
   const videoRef = useRef(null);
   const [isPlaying, setIsPlaying] = useState(true);
@@ -29,16 +30,38 @@ function VideoBanner() {
 
   return (
     <div className="video-banner">
-      <video ref={videoRef} autoPlay loop muted={isMuted}>
+      <video 
+        ref={videoRef} 
+        autoPlay 
+        loop 
+        muted={isMuted}
+        aria-describedby="videoDescription"
+      >
         <source src={Video} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
+      <p id="videoDescription" className="visually-hidden">
+        Description of video content for screen readers.
+      </p>
       <div className="video-controls">
-        <button onClick={togglePlayPause}>
+        <button 
+          onClick={togglePlayPause}
+          aria-label={isPlaying ? "Pause video" : "Play video"}
+        >
           {isPlaying ? "Pause" : "Play"}
         </button>
-        <button onClick={toggleMute}>{isMuted ? "Unmute" : "Mute"}</button>
-        <button onClick={stopVideo}>Stop</button>
+        <button 
+          onClick={toggleMute}
+          aria-label={isMuted ? "Unmute video" : "Mute video"}
+        >
+          {isMuted ? "Unmute" : "Mute"}
+        </button>
+        <button 
+          onClick={stopVideo}
+          aria-label="Stop video"
+        >
+          Stop
+        </button>
       </div>
     </div>
   );
