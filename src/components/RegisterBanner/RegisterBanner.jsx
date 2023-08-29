e03f306a9b682624acc706683d79f9a5c722d697
KaviKurtosys <kaveshan.kistan@kurtosys.com>
2023-08-29 08:08:56 +0200

    Register Banner Re-Work

--- src/components/RegisterBanner/RegisterBanner.jsx
+++ src/components/RegisterBanner/RegisterBanner.jsx
@@ -14,47 +14,37 @@ function RegisterBanner() {
   }
   return (
     <div className="animated-banner">
-      <Row justify="center">
-        <Col xs={22} md={18} lg={16}>
-          <Row>
-            <Col xs={24} md={18}>
-              <Space>
-                <Text className="text" role="heading" aria-level="2">
-                  Participate in Our Upcoming Informational Webinar Session!
-                </Text>
-              </Space>
-            </Col>
-            <Col xs={24} md={6}>
-              <Space align="center">
-                <Button
-                  size="large"
-                  icon={<ArrowRightOutlined />}
-                  style={{
-                    background: "transparent",
-                    color: `${colors.white}`,
-                  }}
-                  className={'register-button icon-animate'}
-                  aria-label="Register for the webinar"
-                >
-                  Register Now
-                </Button>
-              </Space>
-            </Col>
-          </Row>
+      <Row>
+        <Col xs={24} md={24} lg={24} xl={24}>
+          <Text className="text" role="heading" aria-level="2">
+            Participate in Our Upcoming Informational Webinar Session!
+          </Text>
+          <Button
+            size="large"
+            icon={<ArrowRightOutlined />}
+            style={{
+              background: "transparent",
+              color: `${colors.white}`,
+            }}
+            className={"register-button icon-animate"}
+            aria-label="Register for the webinar"
+          >
+            Register Now
+          </Button>
+          <Button
+            icon={<CloseOutlined />}
+            type="text"
+            style={{
+              background: "transparent",
+              color: `${colors.white}`,
+              right: 0,
+              position: "absolute",
+              top: "0",
+            }}
+            aria-label="Close banner"
+            onClick={() => setIsVisible(false)}
+          />
         </Col>
-        <Button
-          icon={<CloseOutlined />}
-          type="text"
-          style={{
-            background: "transparent",
-            color: `${colors.white}`,
-            right: 0,
-            position: "absolute",
-            top: "0",
-          }}
-          aria-label="Close banner"
-          onClick={() => setIsVisible(false)}
-        />
       </Row>
     </div>
   );
